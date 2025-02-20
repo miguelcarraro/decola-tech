6c1313b5e2a3979d4518735bf83ab4533e712ee3
miguelcarraro <miguelcarraromoya@gmail.com>
2025-02-20 01:20:02 -0300

    adição da classe Remetente para facilitar o acesso aos dados pelo app

---
+++ SpringBoot/mensagem-sistema/src/main/java/dio/mensagem_sistema/app/Remetente.java
@@ -0,0 +1,43 @@
+package dio.mensagem_sistema.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@Configuration
+@ConfigurationProperties(prefix = "remetente")
+public class Remetente {
+
+    private String nome;
+    private String email;
+
+    public List<Long> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Long> telefones) {
+        this.telefones = telefones;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private List<Long> telefones;
+
+}
--- SpringBoot/mensagem-sistema/src/main/java/dio/mensagem_sistema/app/SistemaMensagem.java
+++ SpringBoot/mensagem-sistema/src/main/java/dio/mensagem_sistema/app/SistemaMensagem.java
@@ -1,5 +1,6 @@
 package dio.mensagem_sistema.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,22 +11,14 @@ import java.util.List;
 
 @Component
 public class SistemaMensagem implements CommandLineRunner {
-
-    @Value("${nome}")
-    private String nome;
-
-    @Value("${email}")
-    private String email;
-
-    @Value("${telefones}")
-    private List<Long> telefones =
-            new ArrayList<>(Arrays.asList(new Long[]{119543214L}));
+    @Autowired
+    private Remetente remetente;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome
-                + "\nE-mail: " + email
-                + "\nTelefones:" + telefones);
+        System.out.println("Mensagem enviada por: " + remetente.getNome()
+                + "\nE-mail: " + remetente.getEmail()
+                + "\nTelefones:" + remetente.getTelefones());
         System.out.println("Seu cadastro foi aprovado!");
     }
 }
